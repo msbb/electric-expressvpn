5bc80a8f5ce58deba669c56430c504c0fa13bcf9
Maxim Buur <mail@maximbuur.nl>
2022-08-29 16:01:39 +0200

    full tray functionality

--- src/app/core/services/tray/tray.service.ts
+++ src/app/core/services/tray/tray.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { ipcRenderer, webFrame } from 'electron';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
+import * as path from 'path';
 import { ExpresssvpnService } from '../expressvpn/expressvpn.service';
 import { BehaviorSubject, combineLatest, Subscription } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
@@ -13,27 +15,27 @@ export class TrayService {
   webFrame: typeof webFrame;
   childProcess: typeof childProcess;
   fs: typeof fs;
+  path: typeof path;
   tray: any;
   menu: any;
 
   private statusSubscription!: Subscription;
 
-  private readonly favicons = {
-    default: 'src/assets/icons/favicon.png',
-    connected: 'src/assets/icons/favicon-connected.png',
-    disconnected: 'src/assets/icons/favicon-disconnected.png',
-    notactivated: 'src/assets/icons/favicon-notactivated.png',
-    notinstalled: 'src/assets/icons/favicon-notinstalled.png',
-  };
+  private favicons!: { [key: string]: string };
 
   private readonly currentFavicon$ = new BehaviorSubject<string>('');
 
-  constructor(private readonly expressvpnService: ExpresssvpnService) {
+  constructor(
+    private readonly expressvpnService: ExpresssvpnService,
+    private readonly translateService: TranslateService
+  ) {
     if (this.isElectron) {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.webFrame = window.require('electron').webFrame;
 
       this.fs = window.require('fs');
+
+      this.path = window.require('path');
 
       this.childProcess = window.require('child_process');
       this.childProcess.exec('node -v', (error, stdout, stderr) => {
@@ -48,6 +50,7 @@ export class TrayService {
         console.log(`stdout:\n${stdout}`);
       });
 
+      this.setTrayIcons();
       this.openTray();
     }
   }
@@ -56,23 +59,22 @@ export class TrayService {
     return !!(window && window.process && window.process.type);
   }
 
+  private setTrayIcons(): void {
+    this.favicons = {
+      default: this.path.join('src', 'assets', 'icons', 'favicon.png'),
+      connected: 'src/assets/icons/favicon-connected.png',
+      disconnected: 'src/assets/icons/favicon-disconnected.png',
+      notactivated: 'src/assets/icons/favicon-notactivated.png',
+      notinstalled: 'src/assets/icons/favicon-notinstalled.png',
+    };
+  }
+
   private openTray(): void {
     const { Tray } = window.require('@electron/remote');
     const { Menu } = window.require('@electron/remote');
     this.tray = new Tray(this.favicons.default);
     this.currentFavicon$.next(this.favicons.default);
-    this.menu = Menu.buildFromTemplate([
-      {
-        label: 'Item1',
-        type: 'normal',
-        click: () => this.handleTrayClick('item1'),
-      },
-      {
-        label: 'Item2',
-        type: 'normal',
-        click: () => this.handleTrayClick('item2'),
-      },
-    ]);
+    this.menu = Menu.buildFromTemplate([]);
     this.tray.setContextMenu(this.menu);
     this.openStatusSubscription();
   }
@@ -95,42 +97,123 @@ export class TrayService {
       this.expressvpnService.isConnected$,
       this.expressvpnService.isInstalled$,
       this.expressvpnService.isActivated$,
-    ]).subscribe(([currentFavicon, isConnected, isInstalled, isActivated]) => {
-      const favicons = this.favicons;
+      this.expressvpnService.connectedToMessage$,
+    ]).subscribe(
+      ([
+        currentFavicon,
+        isConnected,
+        isInstalled,
+        isActivated,
+        connectedToMessage,
+      ]) => {
+        const favicons = this.favicons;
 
-      if (!isInstalled && currentFavicon !== favicons.notinstalled) {
-        this.setTrayImage(favicons.notinstalled);
-        this.setCurrentFavicon(favicons.notinstalled);
-      }
+        if (!isInstalled && currentFavicon !== favicons.notinstalled) {
+          this.setTrayImage(favicons.notinstalled);
+          this.setCurrentFavicon(favicons.notinstalled);
+          this.setNotInstalledMenu();
+        }
 
-      if (
-        isInstalled &&
-        !isActivated &&
-        currentFavicon !== favicons.notactivated
-      ) {
-        this.setTrayImage(favicons.notactivated);
-        this.setCurrentFavicon(favicons.notactivated);
-      }
+        if (
+          isInstalled &&
+          !isActivated &&
+          currentFavicon !== favicons.notactivated
+        ) {
+          this.setTrayImage(favicons.notactivated);
+          this.setCurrentFavicon(favicons.notactivated);
+          this.setNotActivatedMenu();
+        }
 
-      if (
-        isInstalled &&
-        isActivated &&
-        isConnected &&
-        currentFavicon !== favicons.connected
-      ) {
-        this.setTrayImage(favicons.connected);
-        this.setCurrentFavicon(favicons.connected);
-      }
+        if (
+          isInstalled &&
+          isActivated &&
+          isConnected &&
+          currentFavicon !== favicons.connected
+        ) {
+          this.setTrayImage(favicons.connected);
+          this.setCurrentFavicon(favicons.connected);
+          this.setConnectedMenu(connectedToMessage);
+        }
 
-      if (
-        isInstalled &&
-        isActivated &&
-        !isConnected &&
-        currentFavicon !== favicons.disconnected
-      ) {
-        this.setTrayImage(favicons.disconnected);
-        this.setCurrentFavicon(favicons.disconnected);
+        if (
+          isInstalled &&
+          isActivated &&
+          !isConnected &&
+          currentFavicon !== favicons.disconnected
+        ) {
+          this.setTrayImage(favicons.disconnected);
+          this.setCurrentFavicon(favicons.disconnected);
+          this.setDisconnectedMenu();
+        }
       }
-    });
+    );
+  }
+
+  private setConnectedMenu(connectedToMessage: string): void {
+    const { Menu } = window.require('@electron/remote');
+
+    this.menu = Menu.buildFromTemplate([
+      {
+        label: connectedToMessage,
+        type: 'normal',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: this.translateService.instant('disconnect'),
+        type: 'normal',
+        click: () => this.expressvpnService.disconnect(),
+      },
+    ]);
+
+    this.tray.setContextMenu(this.menu);
+  }
+
+  private setDisconnectedMenu(): void {
+    const { Menu } = window.require('@electron/remote');
+
+    this.menu = Menu.buildFromTemplate([
+      {
+        label: this.translateService.instant('disconnected'),
+        type: 'normal',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: this.translateService.instant('quickConnect'),
+        type: 'normal',
+        click: () => this.expressvpnService.quickConnect(),
+      },
+    ]);
+
+    this.tray.setContextMenu(this.menu);
+  }
+
+  private setNotInstalledMenu(): void {
+    const { Menu } = window.require('@electron/remote');
+
+    this.menu = Menu.buildFromTemplate([
+      {
+        label: this.translateService.instant('notInstalled'),
+        type: 'normal',
+      },
+    ]);
+
+    this.tray.setContextMenu(this.menu);
+  }
+
+  private setNotActivatedMenu(): void {
+    const { Menu } = window.require('@electron/remote');
+
+    this.menu = Menu.buildFromTemplate([
+      {
+        label: this.translateService.instant('notActivated'),
+        type: 'normal',
+      },
+    ]);
+
+    this.tray.setContextMenu(this.menu);
   }
 }
